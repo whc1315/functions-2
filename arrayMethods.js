6c8d622f97a725256a635027db8b2d7dcc686575
William Cuthbertson <whc1315@gmail.com>
2022-01-12 13:41:20 -0700

    5 down

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,7 +18,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers  = mixedNumbers.filter((el, ind) => {
+  return el % 2 === 0;
+});
+console.log(evenNumbers)
 
 
 
@@ -39,8 +42,11 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+const postTaxPrices = prices.map((el) => {
+  el *= 1.07;  
+  return Number(el.toFixed(2))
+});
+console.log(postTaxPrices)
 
 
 ////////// PROBLEM 3 //////////
@@ -57,8 +63,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+const totalPopulation = populations.reduce((acc, curr) => acc + curr)
+console.log(totalPopulation)
 
 
 ////////// PROBLEM 4 //////////
@@ -82,7 +88,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+const myStrongest = monstersInYourPocket.filter((el) => el.CP > 200);
+console.log(myStrongest)
 
 
 
@@ -101,7 +108,15 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 
 // CODE HERE
 
+const taxed = (num1, num2) => ((num1 * num2) + num1)
 
+const orderTotal = orders.map((el) => {
+  const total = taxed(el.price, el.tax)
+  el.total = total
+  return el
+})
+
+console.log(orderTotal)
 
 ////////// PROBLEM 6 //////////
 
@@ -120,3 +135,4 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+
